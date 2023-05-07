3e850506df0223d31fe32ef5f191d2a64af7240b
Terr <terr2048@gmail.com>
2023-05-07 17:48:08 +0300

    fix consecutive rollback of multiple players

--- src/main/h/TileHistory.h
+++ src/main/h/TileHistory.h
@@ -6,7 +6,7 @@
 class TileHistory {
 public:
     void Record(const TileState& state) {
-        if (std::optional<TileState> last = LastValid(); last && state.BlockRotationConfigEquals(*last))
+        if (std::optional<TileState> last_valid = LastValid(); last_valid && state.BlockRotationConfigEquals(*last_valid))
             return; // don't stack the same states
         states_.push_back(state);
     }
@@ -33,20 +33,18 @@ public:
     std::optional<TileState> Rollback(const std::optional<player_t_>& player, int teams, time_t_ time, RollbackFlags flags) {
         if (states_.empty())
             return std::nullopt; // nothing to rollback
-        if (states_.rbegin()->valid) {
-            if (player && *player != states_.rbegin()->player)
-                return std::nullopt; // the last change was not made by this player
-            if (time >= states_.rbegin()->time)
-                return std::nullopt; // the last change was made too long ago
-            if (!(1 << states_.rbegin()->team & teams))
-                return std::nullopt; // team ignored
-        }
+        if (player && !RollbackRequiredPlayer(*player))
+            return std::nullopt; // the last change was not made by this player
+        if (!RollbackRequiredTime(time))
+            return std::nullopt; // the last change was made too long ago
+        if (!RollbackRequiredTeam(teams))
+            return std::nullopt; // team ignored
 
         TileState* ret = nullptr;
         for(auto it = states_.rbegin(); it != states_.rend(); it++) {
-            if (player && *player != it->player || time >= it->time) {
+            if (it->valid && player && it->player != *player || it->time <= time) {
                 if (!ret)
-                    ret = &*it; // save the first suitable state
+                    ret = &*it; // rollback to first suitable state
                 if (it->valid)
                     break; // terminate stack unwinding if a suitable valid state is found
             }
@@ -65,12 +63,38 @@ public:
     }
 
 private:
+    std::optional<TileState> Last() const {
+        if (states_.empty())
+            return std::nullopt;
+        return *states_.rbegin();
+    }
+
     std::optional<TileState> LastValid() const {
         for(auto it = states_.rbegin(); it != states_.rend(); it++) {
             if (it->valid)
                 return *it;
         }
         return std::nullopt;
+    }
+
+    bool RollbackRequiredPlayer(player_t_ player) const {
+        for(auto it = states_.rbegin(); it != states_.rend(); it++) {
+            if (it->player == player)
+                return true;
+            if (it->valid)
+                return false;
+        }
+        return false;
+    }
+
+    bool RollbackRequiredTime(time_t_ time) const {
+        std::optional<TileState> last = Last();
+        return last && (!last->valid || last->time > time);
+    }
+
+    bool RollbackRequiredTeam(int teams) const {
+        std::optional<TileState> last_valid = LastValid();
+        return !last_valid || 1 << last_valid->team & teams;
     }
 
     std::vector<TileState> states_;
